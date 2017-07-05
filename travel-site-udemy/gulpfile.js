a6d8c6ba9f41474c48acd0e58dd1408d027cab23
isAndy99 <claudiu.at@gmail.com>
2017-07-05 23:28:13 +0300

    Finish build task

--- travel-site-udemy/gulpfile.js
+++ travel-site-udemy/gulpfile.js
@@ -1,5 +1,6 @@
 require('./gulp/tasks/styles');
 require('./gulp/tasks/watch');
-require('./gulp/tasks/sprites.js');
-require('./gulp/tasks/scripts.js');
-require('./gulp/tasks/modernizr.js');
+require('./gulp/tasks/sprites');
+require('./gulp/tasks/scripts');
+require('./gulp/tasks/modernizr');
+require('./gulp/tasks/build');
